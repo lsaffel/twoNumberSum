8ced08690ea1ee845bc46c52e7ff509123be1927
Lisa <llvvss@icloud.com>
2022-07-23 15:07:04 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Two Number Sum <br/>
+Write a function that takes in a non-empty array of distinct integers and an integer representing a target sum. <br/>
+If any two numbers in the input array sum up to the target sum, the function should return them in an array, in any order. <br/>
+If no two numbers sum up to the target sum, the function should return an empty array. <br/>
+<br/>
+Note that the target sum has to be obtained by summing two different integers in the array. You can't add a single integer to itself to obtain the target sum, <br/>
+<br/>
+You can assume that there will be at most one pair of numbers summing up to the target sum. <br/>
+<br/>
+
---
+++ main.py
@@ -0,0 +1,76 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def twoNumberSum(array, targetSum):
+    # Write your code here.
+
+    # find the length of the list, into variable arrayLength
+    arrayLength = len(array)
+    lastIndex = arrayLength - 1
+
+    # list indexed from 0 to (arrayLength-1) = from 0 to lastIndex
+
+    # description says it's a non-empty array
+    # if the length of the list is 1, return an empty list
+
+    if arrayLength == 1:
+        returnArray = []
+        return returnArray
+
+    # write a second function that creates an array from the two indexes passed to it ?
+    #
+
+    # else continue, we have a list of size 2 or more
+    # initialize a variable to sumNotfound is false
+    targetSumFound = False
+
+    # sort the list
+    # not necessary to do this unless I have some efficiency built into this somehow
+    # array.sort()   # changes the array, puts the integers in sorted order
+
+    leftIndex = 0  # initialize to the first element
+    # rightIndex = 1 # initialize to the second element
+
+    while not targetSumFound and leftIndex < lastIndex:
+        # check pairs of numbers, comparing their sum to the targetSum
+        # if they are the same, set targetSumFound to True
+
+        rightIndex = leftIndex + 1
+
+        # compare pairs of numbers, comparing leftIndex to leftIndex + rightIndex
+        while not targetSumFound and rightIndex <= lastIndex:
+            print("left index is: ", leftIndex, "right index is: ", rightIndex)
+            testSum = array[leftIndex] + array[rightIndex]
+            if testSum == targetSum:
+                targetSumFound = True
+            else:
+                rightIndex += 1
+
+        # all pairs with element leftIndex have been checked,
+        # so increment leftIndex
+        if not targetSumFound:
+            leftIndex += 1
+
+    # if targetSumFound, then return an array of the values at left and right index
+    # else return an empty array
+
+    if targetSumFound:
+        returnArray = []
+        returnArray.append(array[leftIndex])
+        returnArray.append(array[rightIndex])
+        return returnArray
+    else:
+        returnArray = []
+        return returnArray
+    #
+    pass
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
